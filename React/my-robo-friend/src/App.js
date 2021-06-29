2dbd148252a9a4b7cbe7b06170d19f6c40cd53f1
suman2020 <magarsuman2020@gmail.com>
2021-06-29 17:08:05 -0500

    made some functionality changes in my robo app

--- React/my-robo-friend/src/App.js
+++ React/my-robo-friend/src/App.js
@@ -1,35 +1,51 @@
-import React from 'react';
+ import React from 'react';
 import CardList from './CardList';
-import { Robots } from './robots';
+// import { Robots } from './robots';
 import SearchBox from './SearchBox';
 
 
-
+// class always has a render that returns something
 class App extends React.Component{
 	constructor(){
-		super();
+		super(); // cause we are using the properties of Component: the parent
+		// STATE: an object that describes our application
 		this.state = {
-			robots: Robots,
+			robots: [],
 			searchField : ''
 		}
+		console.log('I am constructor');
 	}
 
 	onSearchChange= (event)=>{
+		// not this.state.searchfield = ..... )(must always use setstate when there is change in state eg. the value inside searchfield)
 		this.setState({searchField: event.target.value});
+	}
+	componentDidMount()
+	{
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response =>response.json())
+		.then(users => this.setState({robots:users}))
+
+		console.log('Helllo what up. this is ComposeDidMount()')
 	}
 
 	render(){
 		const filterRobots = this.state.robots.filter(robot =>{
 			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
 		})
+		console.log("Render");
 		return (
+
 			<div className = 'tc'>
 				<h1 > Robot Friends </h1>
-				<SearchBox searchChange = {this.onSearchChange} />
+				<SearchBox searchChange = {this.onSearchChange} /> 
+
 				<CardList Robots = {filterRobots} />
 			</div>
 			);
+	}
 }
-}
+
+
 
 export default App;
--- React/my-robo-friend/src/CardList.js
+++ React/my-robo-friend/src/CardList.js
@@ -5,6 +5,7 @@ import Card from './Card';
 const CardList = ({Robots}) =>
 {
 	const CardComponent = Robots.map((user,i) =>{
+		// while using loop must include unique "key"
 		return (<Card key = {i} id = {user.id} name = {user.name} email = {user.email }/>)
 	})
 	return(
