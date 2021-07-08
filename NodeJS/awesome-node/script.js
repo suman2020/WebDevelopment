3d737de0bc800e5867b9a345ae422d51dab89ceb
suman2020 <magarsuman2020@gmail.com>
2021-07-07 22:52:05 -0500

    added a test file for Santas Node

---
+++ NodeJS/awesome-node/script.js
@@ -0,0 +1,40 @@
+
+const fs = require('fs')// filesystem
+
+//READ
+fs.readFile('./hello.txt',(err,data)=>{ // read the file asynchronoulsy
+	console.time('total_execution_time')
+	if(err){
+		console.log(err);
+
+	}
+	console.log('1 Async', data.toString('utf8'));// uses the encoding utf8 by default
+	console.timeEnd('total_execution_time')
+})
+
+const file = fs.readFileSync('./hello.txt');  // read the file synchronously
+console.log('2 Sync',file.toString())
+
+
+// APPEND
+fs.appendFile('./hello.txt',' This is nnot soo cool ', err => {
+	if(err){
+		console.log(err);
+	}
+})
+
+
+//WRITE
+fs.writeFile('./bye.txt', 'Sad to see you go', err =>{
+	if(err){
+		console.log(err);
+	}
+})
+
+//DELETE
+fs.unlink('./bye.txt', err=>{
+	if(err){
+		console.log(err)
+	}
+	console.log('Inception')
+})
