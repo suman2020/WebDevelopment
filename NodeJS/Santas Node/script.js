fece20964abce13572315ed5667569eb2e759298
suman2020 <magarsuman2020@gmail.com>
2021-07-08 00:00:45 -0500

    completed Santas Node problem

---
+++ NodeJS/Santas Node/script.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+
+fs.readFile('./test.txt',(err,data)=>{
+	if(err){
+		console.log(err);
+	}
+	if(data){
+		let up_value = 0;
+		let down_value = 0;
+		info = data.toString();
+		info.split('').forEach(ch =>{
+			if(ch === '('){
+				up_value +=1;
+			}
+			if(ch===')'){
+				down_value +=1;
+			}
+		})
+
+		const final_value = up_value - down_value;
+		console.log(final_value);
+	}
+	}
+
+)
---
+++ NodeJS/Santas Node/script2.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+
+function question2(){
+	fs.readFile('./test.txt', (err,data) =>{
+		if(err){
+		console.log(err);
+		}
+		if(data){
+			const information = data.toString();
+			const information_array = information.split('');
+			let accumulator = 0;
+			let counter = 0;
+			const answer = information_array.some(ch =>{
+				if(ch === '('){
+				accumulator+=1;
+				}
+				else if(ch===')'){
+					accumulator-=1;
+				}
+				
+				counter ++;
+				return accumulator < 0;
+
+		})
+
+		console.log('Basement entered at : ', counter);
+
+	}	
+})
+}
+
+question2();
